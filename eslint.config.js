61428b19bb70b01b07cd41d5b7f3fba9f04bb471
NutchapolSal <nutchapol.sal@gmail.com>
2026-09-21 11:53:17 +0700

    update packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from "@eslint/js"
+import { defineConfig } from "eslint/config"
 import eslintConfigPrettier from "eslint-config-prettier"
 import { importX } from "eslint-plugin-import-x"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config([
+export default defineConfig([
     {
         ignores: [
             // Logs
